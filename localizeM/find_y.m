57e3079536c70a6c6ff6f39b88cae8dcc3abb8c6
Kevin Lu <lukevin13@gmail.com>
2016-11-16 16:37:54 -0500

    Done

--- localizeM/find_y.m
+++ localizeM/find_y.m
@@ -1,6 +1,7 @@
 function [n, s] = find_y(x,y)
     ndata = size(x,1);
-    
+
+    % Create matrix to hold points and pairwise distances
     pts = [x,y];
     d_c = zeros(nchoosek(ndata,2),3);
     k = 1;
@@ -12,16 +13,22 @@ function [n, s] = find_y(x,y)
             k = k + 1;
         end
     end
-    
+
+    % Sort the distance matrix from greatest to least
     [~, i_c] = sort(d_c(:,1),'descend');
     
+    % Possible values for north and south stars
     ns1 = d_c(i_c(1), 2);
     ns2 = d_c(i_c(1), 3);
-    
+
+    % Calculate center of mass of the points
     c = [mean(x) mean(y)];
+    
+    % Calculate distance from center of mass to possible north
     d1 = pdist([c;pts(ns1,:)],'euclidean');
     d2 = pdist([c;pts(ns2,:)],'euclidean');
-    
+
+    % North star should always be closer to the center of mass
     if (d1 < d2)
         n = ns1;
         s = ns2;
@@ -29,5 +36,4 @@ function [n, s] = find_y(x,y)
         n = ns2;
         s = ns1;
     end
-    
 end
--- localizeM/localizeMe.m
+++ localizeM/localizeMe.m
@@ -2,7 +2,8 @@ close all;
 clear all;
 
 % Load data
-load('A.mat');
+datafile = 'C.mat';
+load(datafile);
 data = rawStarData;
 x_data = data(:,1:4);
 y_data = data(:,5:8);
@@ -13,11 +14,12 @@ y_check = y_data == 1023;
 data_check = x_check & y_check;
 numStars = 4-sum(data_check,2);
 ndata = size(data,1);
-x_data = x_data - 512;
-y_data = y_data - 512;
+x_data = 512 - x_data;
+y_data = 512 - y_data;
 
 % Calculate position
 r_pos = NaN*zeros(ndata,2);
+r_dir = NaN*zeros(ndata,2);
 for t = 1:ndata
     if (numStars(t) >= 3)
         % Find north and south stars
@@ -26,7 +28,7 @@ for t = 1:ndata
         y = y_data(t,mask)';
         [ind_n, ind_s] = find_y(x,y);
         
-        % Caluclate signed angle between robot y_axis and world y-axis
+        % Calculate signed angle between robot axis and world axis
         northStar = [x(ind_n) y(ind_n)];
         southStar = [x(ind_s) y(ind_s)];
         world_y = northStar - southStar;
@@ -35,21 +37,30 @@ for t = 1:ndata
         theta = robot_theta - world_theta;
         
         % Create Transformation Matrices
-        R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
         T = -[mean([x(ind_n) x(ind_s)]); mean([y(ind_n) y(ind_s)])];
-        S = [1; 14.5/(world_y/2)];
+        R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+        S = 14.5/norm(world_y/2)*ones(2,1);
         
         % Calculate robot position
         r_pos(t,:) = ((R*T).*S)';
+        r_dir(t,:) = (R*[1;0])';
     end 
 end
 
-% Plot path
-figure(1); title('Robot Path');
-plot(r_pos(:,1),r_pos(:,2),'r-');
-% axis([-550 550 -550 550]);
+% Calculate direction
+if (~strcmp(datafile,'A.mat'));
+    r_dir = r_pos - [r_pos(1,:);r_pos(1:end-1,:)];
+end;
+% r_dir = 5*r_dir ./ repmat(sqrt(sum(r_dir.^2,2)),1,2);
 
-% % Plotting
+% Plot path
+res = 1:2:ndata;
+figure(1); title('Robot Path - C.mat'); hold on; grid on;
+% plot(r_pos(:,1),r_pos(:,2),'r-');
+quiver(r_pos(res,1),r_pos(res,2),r_dir(res,1),r_dir(res,2),5);
+axis([-150 150 -80 80]);
+
+% % Plotting Stars
 % figure;
 % for t = 1:ndata
 %     if (numStars(t) >= 3)
