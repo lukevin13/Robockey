7c24320c00eaa013d3af9894c26d173d0c3a46bb
Kevin Lu <lukevin13@gmail.com>
2016-11-17 13:47:15 -0500

    Moved localize to m2 folder. Compiled successfully

---
+++ qualifiersCode/atmega/inc/localize.h
@@ -0,0 +1,9 @@
+#include <math.h>
+#include <stdio.h>
+
+double pdist(double* pt1, double*pt2);
+int nchoosek(int numStars);
+int max_index(double* distances, int n);
+double mean(double* data, int n);
+double mean_i(int* data, int n);
+int localize_me(double* r_pos, int* x, int* y, int numStars);
